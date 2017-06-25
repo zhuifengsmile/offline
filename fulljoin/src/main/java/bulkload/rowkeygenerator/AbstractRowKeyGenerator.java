837c6870bbbbb49c4dfcfbb587dc8c35b00a950c
zhuifengsmile <zhangkuismile@gmail.com>
2017-06-25 23:47:43 +0800

    fix check rowkey param

--- fulljoin/src/main/java/bulkload/rowkeygenerator/AbstractRowKeyGenerator.java
+++ fulljoin/src/main/java/bulkload/rowkeygenerator/AbstractRowKeyGenerator.java
@@ -24,13 +24,13 @@ public abstract class AbstractRowKeyGenerator implements IRowKeyGenerator{
 
     @Override
     public byte[] genRowKey(String id, String param) {
-        Preconditions.checkArgument(StringUtil.isEmpty(param),
-                "rowKey handler params error:" + param + ",should like 4:1000");
+        Preconditions.checkArgument(StringUtil.isNotEmpty(param),
+                "rowKey handler params error:" + param + ",should like 1000:4");
         String[] params = param.split(":");
         Preconditions.checkArgument( 2 != params.length,
-                "rowKey handler params error:" + param + ",should like 4:1000");
-        int prefixNum = Integer.parseInt(params[0]);
-        int mod = Integer.parseInt(params[1]);
+                "rowKey handler params error:" + param + ",should like 1000:4");
+        int mod = Integer.parseInt(params[0]);
+        int prefixNum = Integer.parseInt(params[1]);
         return genRowKey(id, prefixNum, mod);
     }
 }
